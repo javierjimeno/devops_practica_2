1ac701334c624a5144bb7470663f6d8382ebf23c
javierjimeno <javierjimenov@gmail.com>
2021-06-30 14:38:08 +0000

    otro

---
+++ terraform/correccion-vars.tf
@@ -0,0 +1,26 @@
+variable "location" {
+  type = string
+  description = "Región de Azure donde crearemos la infraestructura"
+  default = "West Europe"
+}
+
+variable "storage_account" {
+  type = string
+  description = "Nombre para la storage account"
+  default = "jjvsstorageaccount1"
+}
+variable "public_key_path" {
+  type = string
+  description = "Ruta para la clave pública de acceso a las instancias"
+  default = "~/.ssh/id_rsa.pub" # o la ruta correspondiente
+}
+variable "vm_size" {
+  type = string
+  description = "Tamaño de la máquina virtual"
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+}
+variable "ssh_user" {
+  type = string
+  description = "Usuario para hacer ssh"
+  default = "<SSH USER>"
+}
