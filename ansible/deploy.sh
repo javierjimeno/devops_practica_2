a035d6d093b3fb1b507418a367812437c1ad3d14
javierjimeno <javierjimenov@gmail.com>
2021-07-13 20:04:29 +0000

    despliegue kubernetes

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,8 +1,23 @@
-# terraform desde CLI de azure
-# terraform init
-# terraform apply
-
-# ejecutar ansible desde nodo master
-#conectarse a través de SSH
 
 #!/bin/bash
+
+# configuración común para todos los nodos
+ansible-playbook -i hosts 01-configuracion-comun.yaml
+
+# configuración del nodo NFS
+ansible-playbook -i hosts 02-configuracion-nfs.yaml
+
+# condiguracion nodos master y workers
+ansible-playbook -i hosts 03-configuracion-nodos.yaml
+
+# configuración de kubernetes en nodos master y workers
+ansible-playbook -i hosts 04-configuracion-kubernetes.yaml
+
+# configuración de SDN en nodo master
+ansible-playbook -i hosts 05-configuracion-sdn.yaml
+
+# configuración de nodos workers
+ansible-playbook -i hosts 06-configuracion-workers.yaml
+
+# Instalación de ingress controller en nodo master
+ansible-playbook -i hosts 07-configuracion-ingress.yaml
